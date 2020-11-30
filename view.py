f900648b4bc410670ecc749c0228be492716db8b
Ito-Min <ito.20001004@gmail.com>
2020-11-30 13:33:07 +0900

    add and test view

---
+++ view.py
@@ -0,0 +1,17 @@
+REPLY_ENDPOINT = 'https://api.line.me/v2/bot/message/reply'
+
+def post_text(reply_token, text):
+    header = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer {ENTER_ACCESS_TOKEN}"
+    }
+    payload = {
+          "replyToken":reply_token,
+          "messages":[
+                {
+                    "type":"text",
+                    "text": text
+                }
+            ]
+    }
+    requests.post(REPLY_ENDPOINT, headers=header, data=json.dumps(payload))
